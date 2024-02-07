292e61bf84e959535721efccd9e24dbd25661250
KKKmoji <qwer123456qin@163.com>
2024-02-07 16:08:50 +0800

    Read excel

--- My project/Assets/Tools/ExcelReaderTool.cs
+++ My project/Assets/Tools/ExcelReaderTool.cs
@@ -9,16 +9,51 @@ using ExcelDataReader;
 
 public class ExcelReaderTool : MonoBehaviour
 {
+    private static String _readFloder = null;
+    private static String _writeFloder = null;
+
     [MenuItem("Tools/Excel Reader")]
     public static void ExcelReader()
     {
+        //Get project path
         var curDirectory = Directory.GetCurrentDirectory();
 
-        var readFloder = Directory.GetDirectories(curDirectory, "Excel");
-        var excelContainer = Directory.GetFiles(readFloder[0], "*.xlsx");
+        if (ExcelReaderTool._readFloder == null)
+            _readFloder = Directory.GetDirectories(curDirectory, "Excel")[0];
+        if (ExcelReaderTool._writeFloder == null)
+            _writeFloder = Directory.GetDirectories(curDirectory, "Config")[0];
 
-        var writeFloder = Directory.GetDirectories(curDirectory, "Config");
+        ExcelReaderTool.Work();
+    }
 
-        Debug.LogError(excelContainer.Length);
+    private static void Work()
+    {
+        //Get all excel files
+        var excelFilesNameContainer = Directory.GetFiles(_readFloder, "*.xlsx");
+
+        foreach (var fileName in excelFilesNameContainer)
+        {
+            string excelFilePath = fileName;
+            FileStream excelFile = File.OpenRead(excelFilePath);
+            IExcelDataReader firstSheet = ExcelReaderFactory.CreateReader(excelFile);
+
+            if (firstSheet == null)
+            {
+                Debug.LogError($"{fileName} convert to Excel Sheet fail£¡");
+                continue;
+            }
+
+            ExcelReaderTool.ReadExcelSheet(firstSheet);
+        }
+    }
+
+    private static void ReadExcelSheet(IExcelDataReader Sheet)
+    {
+        //Number of valid columns on worksheet
+        int columnCount = Sheet.FieldCount;
+        //Number of valid rows on worksheet
+        int rowCount = Sheet.RowCount;
+
+        
     }
 }
