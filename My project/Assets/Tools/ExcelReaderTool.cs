909a780ac82873af6809c9a1f92b5183d20d2c60
KKKmoji <qwer123456qin@163.com>
2024-02-19 15:39:21 +0800

    [feature] Read Excel && Write File Init

--- My project/Assets/Tools/ExcelReaderTool.cs
+++ My project/Assets/Tools/ExcelReaderTool.cs
@@ -6,11 +6,12 @@ using System.IO;
 using System.Data;
 using System;
 using ExcelDataReader;
+using System.Text;
 
 public class ExcelReaderTool : MonoBehaviour
 {
-    private static String _readFloder = null;
-    private static String _writeFloder = null;
+    private static String _readFloderPath = null;
+    private static String _writeFloderPath = null;
 
     [MenuItem("Tools/Excel Reader")]
     public static void ExcelReader()
@@ -18,10 +19,10 @@ public class ExcelReaderTool : MonoBehaviour
         //Get project path
         var curDirectory = Directory.GetCurrentDirectory();
 
-        if (ExcelReaderTool._readFloder == null)
-            _readFloder = Directory.GetDirectories(curDirectory, "Excel")[0];
-        if (ExcelReaderTool._writeFloder == null)
-            _writeFloder = Directory.GetDirectories(curDirectory, "Config")[0];
+        if (ExcelReaderTool._readFloderPath == null)
+            _readFloderPath = Directory.GetDirectories(curDirectory, "Excel")[0];
+        if (ExcelReaderTool._writeFloderPath == null)
+            _writeFloderPath = Directory.GetDirectories(curDirectory, "Config")[0];
 
         ExcelReaderTool.Work();
     }
@@ -29,31 +30,81 @@ public class ExcelReaderTool : MonoBehaviour
     private static void Work()
     {
         //Get all excel files
-        var excelFilesNameContainer = Directory.GetFiles(_readFloder, "*.xlsx");
+        var excelFilesNameContainer = Directory.GetFiles(_readFloderPath, "*.xlsx");
 
-        foreach (var fileName in excelFilesNameContainer)
+        ClearConfig();
+
+        foreach (var fileStream in excelFilesNameContainer)
         {
-            string excelFilePath = fileName;
+            string excelFilePath = fileStream;
             FileStream excelFile = File.OpenRead(excelFilePath);
-            IExcelDataReader firstSheet = ExcelReaderFactory.CreateReader(excelFile);
+            IExcelDataReader firstSheet = ExcelReaderFactory.CreateOpenXmlReader(excelFile);
 
             if (firstSheet == null)
             {
-                Debug.LogError($"{fileName} convert to Excel Sheet fail£¡");
+                Debug.LogError($"{fileStream} convert to Excel Sheet fail£¡");
                 continue;
             }
 
-            ExcelReaderTool.ReadExcelSheet(firstSheet);
+            FileStream csFile = new FileStream(FileNameFactory(fileStream), FileMode.Create);
+            Debug.Log($"Create new file :: {csFile.Name}");
+
+            ExcelReaderTool.ReadExcelSheet_WriteCSFile(firstSheet, csFile);
         }
     }
 
-    private static void ReadExcelSheet(IExcelDataReader Sheet)
+    private static void ClearConfig()
+    {
+        //Get all config files
+        var configFilesNameContainer = Directory.GetFiles(_writeFloderPath, "*.*");
+
+        foreach(var config in configFilesNameContainer)
+        {
+            File.Delete(config);
+        }
+    }
+
+    private static string FileNameFactory(string stream)
+    {
+        StringBuilder name = new StringBuilder();
+
+        char[] charArray = stream.ToCharArray();
+        foreach(char c in charArray)
+        {
+            if(c == '\\')
+            {
+                name.Clear();
+                continue;
+            }
+            
+            if(c == '.')
+            {
+                break;
+            }
+
+            name.Append(c);
+        }
+        name.Append("Config.cs");
+
+        return _writeFloderPath + '\\' + name.ToString();
+    }
+
+    private static void ReadExcelSheet_WriteCSFile(IExcelDataReader Sheet, FileStream writeFile)
     {
         //Number of valid columns on worksheet
         int columnCount = Sheet.FieldCount;
         //Number of valid rows on worksheet
         int rowCount = Sheet.RowCount;
 
-        
+        for (int i = 0; i < rowCount; i++)
+        {
+            Sheet.Read();
+            for(int j = 0; j < columnCount; j++)
+            {
+                var v = Sheet.GetValue(j);
+                Debug.Log(v);
+            }
+        }
+
     }
 }
